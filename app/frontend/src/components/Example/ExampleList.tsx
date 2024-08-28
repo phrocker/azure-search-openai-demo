3c34885634a7be8ac6ac23e6d4167dd5ec8294bb
Marc Parisi <phrocker@apache.org>
2024-08-28 08:25:03 -0400

    marc updates

--- app/frontend/src/components/Example/ExampleList.tsx
+++ app/frontend/src/components/Example/ExampleList.tsx
@@ -3,9 +3,9 @@ import { Example } from "./Example";
 import styles from "./Example.module.css";
 
 const DEFAULT_EXAMPLES: string[] = [
-    "What is included in my Northwind Health Plus plan that is not in standard?",
-    "What happens in a performance review?",
-    "What does a Product Manager do?"
+    "What is the president pro tempore?",
+    "How can the vice president become president?",
+    "What rights are bestowed with the constitition?"
 ];
 
 const GPT4V_EXAMPLES: string[] = [
